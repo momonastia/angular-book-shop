d8555beb71b72db45be5137cd8879adcec5575ae
anastasia tuyrikova <anastasiatyrikova@gmail.com>
2023-07-25 23:54:35 +0200

    Feat: books component added

---
+++ src/app/books/books.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books',
+  templateUrl: './books.component.html',
+  styleUrls: ['./books.component.css']
+})
+export class BooksComponent {
+  name = "Clean Code"
+  author = "Robert C Martin"
+}
