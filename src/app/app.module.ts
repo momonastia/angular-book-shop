40f6ee63c4d1f94c7a73282582421352b0e2be45
anastasia tuyrikova <anastasiatyrikova@gmail.com>
2023-08-06 14:53:11 +0300

    Feat: books module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BooksComponent } from './books/books.component';
 import { FormsModule } from '@angular/forms';
-import { BookComponent } from './book/book.component';
+import { BooksModule } from './books/books.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent,
-    BookComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BooksModule
   ],
   providers: [],
   bootstrap: [AppComponent]
