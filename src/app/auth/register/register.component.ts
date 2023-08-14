17baa57153fefc2f0da6da07bf9aa5a16172f1e3
anastasia tuyrikova <anastasiatyrikova@gmail.com>
2023-08-14 14:11:26 +0300

    Chore: fierbase installed

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RegisterForm } from 'src/app/interface/Auth';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 @Component({
   selector: 'app-register',
@@ -15,6 +16,19 @@ export class RegisterComponent {
   }
 
   submit() {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, this.form.email, this.form.password)
+    .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+     })
+    .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+     });
+
     console.log(this.form)
   }
 
---
+++ src/app/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrqoM2SQCZzgVqgZ87ESynxvh1aeAKAAI",
+  authDomain: "angular-book-shop-fa147.firebaseapp.com",
+  projectId: "angular-book-shop-fa147",
+  storageBucket: "angular-book-shop-fa147.appspot.com",
+  messagingSenderId: "1032621486051",
+  appId: "1:1032621486051:web:7567f312cb02beb4333ad6"
+};
+
+// Initialize Firebase
+export const firebase = initializeApp(firebaseConfig);
